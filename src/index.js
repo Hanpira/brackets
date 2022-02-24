9cdafc2a91e8d790f6b799f67f1ad87e5fa7c104
Hanpiranpm <annromania@gmail.com>
2022-02-25 00:33:59 +0300

    feat: add boolean for odd number of brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  if ((str.length + 1) % 2 == 0 ) { /* if str.length is odd number - we can stop here immediately */
+    return false;
+  };
+
 }
